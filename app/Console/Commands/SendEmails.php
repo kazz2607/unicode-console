7539a8e179c5dba78387fb61154dc98034013ee1
kazz2607 <kairu2607@gmail.com>
2024-12-12 22:09:31 +0700

    Bai 186

--- app/Console/Commands/SendEmails.php
+++ app/Console/Commands/SendEmails.php
@@ -19,8 +19,9 @@ class SendEmails extends Command
      *
      * @var string
      */
+    protected $signature = 'email:send'; // Đặt tên câu lệnh
     // protected $signature = 'email:send {email=contact@unicode} {--Q|queue=}'; // Đặt tên câu lệnh
-    protected $signature = 'email:send {email*} {--Q|queue=*}'; // Đặt tên câu lệnh
+    // protected $signature = 'email:send {email*: Email muốn gửi} {name: Tên muốn gửi} {--Q|queue=*: Kích hoạt hàng đợi}'; // Đặt tên câu lệnh
 
     /**
      * The console command description.
@@ -34,8 +35,21 @@ class SendEmails extends Command
      */
     public function handle()
     {
-        var_dump($this->argument('email'));
-        var_dump($this->option('queue'));
-        echo $this->email->send();
+        // var_dump($this->argument());
+        // // var_dump($this->argument('email'));
+        // // var_dump($this->option('queue'));
+        // echo $this->email->send();
+
+        // $name = $this->ask('Vui lòng nhập tên');
+        // $name = $this->anticipate('What is your name?', ['Taylor', 'Dayle']);
+        // var_dump($name);
+        // $password = $this->secret('Vui lòng nhập mật khẩu');
+        // var_dump($password);
+        // $confirm = $this->confirm('Bạn có muốn tiếp tục? [y|N]');
+        // var_dump($confirm);
+
+        $name = $this->choice('What is your name?', ['Taylor', 'Dayle']);
+        $this->info('Tên của bạn là: '.$name);
+        // $this->error('Tên của bạn là: '.$name);
     }
 }
